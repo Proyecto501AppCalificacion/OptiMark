3f418ca72a30cb3ff8dde63646c391db6855dce3
DickeisonBM <optimark37@gmail.com>
2025-03-27 14:41:24 -0500

    encabezado

---
+++ frontend/MiAppFrontend/screens/10_componentes/superior/encabezado_estilos.js
@@ -0,0 +1,27 @@
+const encabezadoEstilos = {
+    encabezadoContainer: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+    },
+    contenedor: {
+      backgroundColor: '#0A1F44',
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 20,
+    },
+    logo: {
+      width: 60,
+      height: 60,
+      marginBottom: 10,
+      resizeMode: 'contain',
+    },
+    titulo: {
+      color: '#00FF00',
+      fontSize: 22,
+      fontWeight: 'bold',
+    },
+  };
+  
+  export default encabezadoEstilos;
