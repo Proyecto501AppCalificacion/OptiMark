e6077c89bc4092164b3eb86cb8e0c2e9b60d11e0
DickeisonBM <optimark37@gmail.com>
2025-04-01 16:43:18 -0500

    mejora encabezado

--- frontend/MiAppFrontend/screens/10_componentes/superior/encabezado.js
+++ frontend/MiAppFrontend/screens/10_componentes/superior/encabezado.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TouchableOpacity, SafeAreaView } from 'react-native';
 import encabezadoEstilos from './encabezado_estilos';
 
-const Encabezado = () => {
+const Encabezado = ({ onLogout }) => {
   return (
-    <View style={encabezadoEstilos.encabezadoContainer}>
+    <SafeAreaView style={encabezadoEstilos.encabezadoContainer}>
       <View style={encabezadoEstilos.contenedor}>
-        <Image source={require('../../../assets/images/logo.png')} style={encabezadoEstilos.logo} />
-        <Text style={encabezadoEstilos.titulo}>OptiMark</Text>
+        
+        {/* Botón de Cerrar sesión */}
+        <TouchableOpacity onPress={onLogout} style={encabezadoEstilos.botonCerrarSesion}>
+          <Text style={encabezadoEstilos.textoCerrarSesion}>Cerrar sesión</Text>
+        </TouchableOpacity>
+
+        {/* Contenedor de logo y título */}
+        <View style={encabezadoEstilos.logoContainer}>
+          <Image source={require('../../../assets/images/logo.png')} style={encabezadoEstilos.logo} />
+          <Text style={encabezadoEstilos.titulo}>OptiMark</Text>
+        </View>
+
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
