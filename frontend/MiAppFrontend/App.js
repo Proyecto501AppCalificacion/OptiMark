e9280806eb6e3c2c092d5713ed4d95ef57af5236
Luis Alejandro Navarro Gutierrez <optimark37@gmail.com>
2025-05-20 13:12:07 -0500

    Cambio a inicio de APP

--- frontend/MiAppFrontend/App.js
+++ frontend/MiAppFrontend/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { useFonts } from 'expo-font';
 import { getData } from './src/services/api';
-import Modreportes  from './screens/8_mod_reportes/mod_reportes';
+import Inicio_de_app  from './screens/1_inicio_de_app/inicio_de_app';
 
 export default function App() {
   const [data, setData] = useState([]);
@@ -37,7 +37,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Modreportes />
+      <Inicio_de_app />
     </View>
   );
 }
