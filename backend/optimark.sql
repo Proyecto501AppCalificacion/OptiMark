e5ec6d9a2bd944065c89cb1d860b0bdfbbb8dedd
Luis Alejandro Navarro Gutierrez <optimark37@gmail.com>
2025-05-21 20:35:15 -0500

    Actualizacion de base  de datos mysql

--- backend/optimark.sql
+++ backend/optimark.sql
@@ -31,7 +31,7 @@ CREATE TABLE `almacenamiento` (
   PRIMARY KEY (`almacenamiento_id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `almacenamiento_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `usuarios` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,6 +40,7 @@ CREATE TABLE `almacenamiento` (
 
 LOCK TABLES `almacenamiento` WRITE;
 /*!40000 ALTER TABLE `almacenamiento` DISABLE KEYS */;
+INSERT INTO `almacenamiento` VALUES (1,1,500,1000,'2025-01-15 10:49:23'),(2,2,700,1000,'2025-01-15 10:49:23'),(3,3,300,1000,'2025-01-15 10:49:23'),(4,4,450,1000,'2025-01-15 10:49:23'),(5,5,600,1000,'2025-01-15 10:49:23');
 /*!40000 ALTER TABLE `almacenamiento` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -58,7 +59,7 @@ CREATE TABLE `escaneos` (
   PRIMARY KEY (`escaneo_id`),
   KEY `examen_id` (`examen_id`),
   CONSTRAINT `escaneos_ibfk_1` FOREIGN KEY (`examen_id`) REFERENCES `examenes` (`examen_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -87,7 +88,7 @@ CREATE TABLE `estudiantes` (
   PRIMARY KEY (`estudiante_id`),
   KEY `examen_id` (`examen_id`),
   CONSTRAINT `estudiantes_ibfk_1` FOREIGN KEY (`examen_id`) REFERENCES `examenes` (`examen_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -109,14 +110,14 @@ DROP TABLE IF EXISTS `examenes`;
 CREATE TABLE `examenes` (
   `examen_id` int NOT NULL AUTO_INCREMENT,
   `materia` varchar(255) NOT NULL,
+  `colegio` varchar(100) NOT NULL,
   `cantidad_preguntas` int NOT NULL,
-  `cantidad_estudiantes` int NOT NULL,
-  `creado_por` int DEFAULT NULL,
-  `creado_en` datetime DEFAULT CURRENT_TIMESTAMP,
+  `creado_por` varchar(255) NOT NULL,
+  `creado_en` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `nombre_examen` varchar(255) NOT NULL,
   PRIMARY KEY (`examen_id`),
-  KEY `creado_por` (`creado_por`),
-  CONSTRAINT `examenes_ibfk_1` FOREIGN KEY (`creado_por`) REFERENCES `usuarios` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `creado_por` (`creado_por`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -125,33 +126,40 @@ CREATE TABLE `examenes` (
 
 LOCK TABLES `examenes` WRITE;
 /*!40000 ALTER TABLE `examenes` DISABLE KEYS */;
+INSERT INTO `examenes` VALUES (1,'Sociales','SENA',2,'Diomedes','2025-05-08 18:02:23','Prueba de examen'),(2,'Programacion','SENA',3,'Henry','2025-05-08 19:18:49','Prueba de sesion 2');
 /*!40000 ALTER TABLE `examenes` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `preguntas`
+-- Table structure for table `preguntas_examen`
 --
 
-DROP TABLE IF EXISTS `preguntas`;
+DROP TABLE IF EXISTS `preguntas_examen`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `preguntas` (
-  `pregunta_id` int NOT NULL AUTO_INCREMENT,
-  `examen_id` int DEFAULT NULL,
-  `pregunta_texto` text NOT NULL,
-  PRIMARY KEY (`pregunta_id`),
-  KEY `examen_id` (`examen_id`),
-  CONSTRAINT `preguntas_ibfk_1` FOREIGN KEY (`examen_id`) REFERENCES `examenes` (`examen_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `preguntas_examen` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `pregunta` text NOT NULL,
+  `opcion_a` varchar(255) DEFAULT NULL,
+  `opcion_b` varchar(255) DEFAULT NULL,
+  `opcion_c` varchar(255) DEFAULT NULL,
+  `opcion_d` varchar(255) DEFAULT NULL,
+  `respuesta_correcta` varchar(255) DEFAULT NULL,
+  `examen_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_examen` (`examen_id`),
+  CONSTRAINT `fk_examen` FOREIGN KEY (`examen_id`) REFERENCES `examenes` (`examen_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `preguntas`
+-- Dumping data for table `preguntas_examen`
 --
 
-LOCK TABLES `preguntas` WRITE;
-/*!40000 ALTER TABLE `preguntas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `preguntas` ENABLE KEYS */;
+LOCK TABLES `preguntas_examen` WRITE;
+/*!40000 ALTER TABLE `preguntas_examen` DISABLE KEYS */;
+INSERT INTO `preguntas_examen` VALUES (1,'2+2','4','1','3','2','A',1),(2,'3+1','4','2','5','7','A',1),(3,'1+1','2','9','11','15','A',2),(4,'2+5','17','77','70','7','D',2),(5,'1+3','4','6','43','65','A',2);
+/*!40000 ALTER TABLE `preguntas_examen` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -172,9 +180,8 @@ CREATE TABLE `respuestas_estudiantes` (
   KEY `estudiante_id` (`estudiante_id`),
   KEY `pregunta_id` (`pregunta_id`),
   CONSTRAINT `respuestas_estudiantes_ibfk_1` FOREIGN KEY (`examen_id`) REFERENCES `examenes` (`examen_id`),
-  CONSTRAINT `respuestas_estudiantes_ibfk_2` FOREIGN KEY (`estudiante_id`) REFERENCES `estudiantes` (`estudiante_id`),
-  CONSTRAINT `respuestas_estudiantes_ibfk_3` FOREIGN KEY (`pregunta_id`) REFERENCES `preguntas` (`pregunta_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `respuestas_estudiantes_ibfk_2` FOREIGN KEY (`estudiante_id`) REFERENCES `estudiantes` (`estudiante_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -204,7 +211,7 @@ CREATE TABLE `usuarios` (
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `correo` (`correo`),
   UNIQUE KEY `cedula` (`cedula`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -213,6 +220,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'Juan Pérez','juan.perez@ejemplo.com','3001234567','123456789','contrasena123','2025-01-15 10:48:28'),(2,'Ana Gómez','ana.gomez@ejemplo.com','3102345678','987654321','contrasena456','2025-01-15 10:48:28'),(3,'Carlos Martínez','carlos.martinez@ejemplo.com','3203456789','112233445','contrasena789','2025-01-15 10:48:28'),(4,'Laura Sánchez','laura.sanchez@ejemplo.com','3304567890','223344556','contrasena101','2025-01-15 10:48:28'),(5,'Pedro López','pedro.lopez@ejemplo.com','3405678901','334455667','contrasena112','2025-01-15 10:48:28');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -225,4 +233,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-01-15 10:26:15
+-- Dump completed on 2025-05-21 20:31:46
